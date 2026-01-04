7539c0cc2351e20b86ae8745e454db2e26d47b1e
me-harsha499 <me.harsha499@gmail.com>
2026-01-04 11:08:48 +0530

    Update app.js

--- app.js
+++ app.js
@@ -8,7 +8,12 @@ const app = express();
 
 // CORS configuration
 app.use(cors({
-  origin: ['http://localhost:3000', 'http://127.0.0.1:3000', 'http://localhost:3002', 'http://127.0.0.1:3002'],
+  origin: ['http://localhost:3000', 
+           'http://127.0.0.1:3000',
+           'http://localhost:3002', 
+           'http://127.0.0.1:3002',
+           'https://www.gprinteriors.com'
+          ],
   credentials: true
 }));
 
